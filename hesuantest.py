7a2edffdadba58a274d8f879f28d5230cc706a32
aimibobi <ccfaimi@foxmail.com>
2020-05-20 17:09:22 +0800

    modified:   hesuantest.py 随即修改

--- hesuantest.py
+++ hesuantest.py
@@ -1,15 +1,22 @@
 '''
 @Author: your name
 @Date: 2020-05-18 20:03:14
-@LastEditTime: 2020-05-19 13:31:36
+@LastEditTime: 2020-05-20 17:08:55
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \opms\hesuantest.py
 '''
 import pandas as pd
-df = pd.read_excel('519hs1.xlsx',sheet_name = 0)
-data = df.head()   #前五条记录
-print ("概览表:\n{0}".format(data))
-data = df.values
-#print ("获取的所有值:\n{0}".format(data))
-print ("输出列标题", df.columns.values)
+import numpy as np
+import time,datetime
+
+hs = pd.DataFrame(pd.read_excel('518hs.xls',sheet_name = 0))
+hs5191 = pd.DataFrame(pd.read_excel('519hs1.xlsx',sheet_name = 0))
+hs5192 = pd.DataFrame(pd.read_excel('519hs2.xlsx',sheet_name = 0))
+hs5193 = pd.DataFrame(pd.read_excel('519hs3.xlsx',sheet_name = 0))
+hs5194 = pd.DataFrame(pd.read_excel('519hs4.xlsx',sheet_name = 0))
+hs5195 = pd.DataFrame(pd.read_excel('519hs5.xlsx',sheet_name = 0))
+
+
+
+
