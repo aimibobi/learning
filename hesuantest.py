a8b37261021a9282501e4d1cb9a694cde1967959
aimibobi <ccfaimi@foxmail.com>
2020-05-22 17:28:32 +0800

    522again

--- hesuantest.py
+++ hesuantest.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-05-18 20:03:14
-@LastEditTime: 2020-05-20 17:08:55
+@LastEditTime: 2020-05-22 17:27:53
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \opms\hesuantest.py
@@ -11,11 +11,7 @@ import numpy as np
 import time,datetime
 
 hs = pd.DataFrame(pd.read_excel('518hs.xls',sheet_name = 0))
-hs5191 = pd.DataFrame(pd.read_excel('519hs1.xlsx',sheet_name = 0))
-hs5192 = pd.DataFrame(pd.read_excel('519hs2.xlsx',sheet_name = 0))
-hs5193 = pd.DataFrame(pd.read_excel('519hs3.xlsx',sheet_name = 0))
-hs5194 = pd.DataFrame(pd.read_excel('519hs4.xlsx',sheet_name = 0))
-hs5195 = pd.DataFrame(pd.read_excel('519hs5.xlsx',sheet_name = 0))
+
 
 
 
