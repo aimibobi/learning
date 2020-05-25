dfa3a774f8c957c71e82c2e0a872a99c46772d06
aimibobi <ccfaimi@foxmail.com>
2020-05-25 17:33:05 +0800

    walksuiji

---
+++ walk.py
@@ -0,0 +1,16 @@
+'''
+@Author: your name
+@Date: 2020-05-25 17:21:46
+@LastEditTime: 2020-05-25 17:27:28
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \opms\walk.py
+'''
+import numpy as np
+
+nsteps = 1000
+draws = np.random.randint(0,2,size=nsteps)
+steps = np.where(draws>0,1,-1)
+walk = steps.cumsum()
+#print (steps)
+print (walk)
