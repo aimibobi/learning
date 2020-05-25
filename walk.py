e78afb00c593f61d97a9a646c9e6c316e2cf8686
aimibobi <ccfaimi@foxmail.com>
2020-05-25 18:02:15 +0800

    walk

--- walk.py
+++ walk.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-05-25 17:21:46
-@LastEditTime: 2020-05-25 17:27:28
+@LastEditTime: 2020-05-25 18:00:45
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \opms\walk.py
@@ -13,4 +13,4 @@ draws = np.random.randint(0,2,size=nsteps)
 steps = np.where(draws>0,1,-1)
 walk = steps.cumsum()
 #print (steps)
-print (walk)
+#print (walk)
